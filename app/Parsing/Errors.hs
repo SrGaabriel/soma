2ef01754e0107313be2e6b3d7eb25e6960fd2164
SrGaabriel <gaabrielms@proton.me>
2025-02-16 17:04:11 -0300

    feat: seamless indentation sensitiveness

--- app/Parsing/Errors.hs
+++ app/Parsing/Errors.hs
@@ -9,16 +9,22 @@ data ParsingError
       , received :: Token 
       }
   | InvalidTokenForType Token
+  | ExpectedIndentation Token -- for when a token isn't indented
+  | ExpectedDifferentIndentation Token Int Int -- for when the indentation is wrong
+  | UnseparatedStatements Token -- for when two statements are on the same line
   | EndOfInput
   | Debug 
   deriving (Eq)
 
 instance Show ParsingError where
-  show (UnexpectedToken t) = "Unexpected token " ++ referenceToken t
-  show (ExpectedDifferentToken tExpected tReceived) = "Expected token '" ++ show tExpected ++ "' but received " ++ referenceToken tReceived
-  show (InvalidTokenForType t) = "Invalid token for type: " ++ referenceToken t
-  show (EndOfInput) = "End of input"
-  show Debug = "Debug"
+    show (UnexpectedToken t) = "Unexpected token " ++ referenceToken t
+    show (ExpectedDifferentToken tExpected tReceived) = "Expected token '" ++ show tExpected ++ "' but received " ++ referenceToken tReceived
+    show (InvalidTokenForType t) = "Invalid token for type: " ++ referenceToken t
+    show (ExpectedIndentation _) = "Expected indentation"
+    show (UnseparatedStatements t) = "Unseparated statements by newline at " ++ referenceToken t
+    show (ExpectedDifferentIndentation _ expc recv) = "Expected indentation of " ++ show expc ++ " spaces but received " ++ show recv
+    show (EndOfInput) = "End of input"
+    show Debug = "Debug"
 
 instance PrintableError ParsingError where
   errorMessage err = show err
@@ -33,5 +39,8 @@ getErrorToken :: ParsingError -> Maybe Token
 getErrorToken (UnexpectedToken t) = Just t
 getErrorToken (ExpectedDifferentToken _ t) = Just t
 getErrorToken (InvalidTokenForType t) = Just t
+getErrorToken (ExpectedIndentation t) = Just t
+getErrorToken (ExpectedDifferentIndentation t _ _) = Just t
+getErrorToken (UnseparatedStatements t) = Just t
 getErrorToken (EndOfInput) = Nothing -- TODO: replace with EOF token
 getErrorToken Debug = Nothing
--- app/Parsing/Parser.hs
+++ app/Parsing/Parser.hs
@@ -116,12 +116,12 @@ parseFunctionBody = do
   incoming <- peek
   case tokenKind incoming of
     TokenNewline -> do
-      cases <- parseIndentedBlock 2 parsePatternMatchCase
-      return $ Expression incoming (PatternMatchExpr cases)
+        cases <- parseIndentedBlock 0 parsePatternMatchCase
+        return $ Expression incoming (PatternMatchExpr cases)
     TokenEquals -> do
-      _equals <- next
-      expression <- parseExpression
-      return expression
+        _ <- next
+        expression <- parseExpression
+        return expression
     _ -> failParser $ UnexpectedToken incoming
 
 parsePatternMatchCase :: Parser Expression
@@ -173,7 +173,8 @@ parseFactor = do
             return numericExpr
         TokenDo -> do
             doToken <- next
-            block <- parseIndentedBlock 2 parseExpression
+            let indent = tokenIndent doToken
+            block <- parseIndentedBlock indent parseExpression
             return $ expr doToken (BlockExpr block)
         _ -> failParser $ UnexpectedToken token
 
@@ -232,15 +233,36 @@ parseFluidSequence  end itemParser = Parser $ \tokens -> do
     parseNext [] tokens
 
 parseIndentedBlock :: Int -> Parser a -> Parser [a]
-parseIndentedBlock minimumIndent itemParser = Parser $ \tokens -> do
-   let parseNext acc remaining = case remaining of
+parseIndentedBlock previousIndent itemParser = Parser $ \tokens -> do
+    indentation <- case tokens of
+        [] -> Left EndOfInput
+        (t@Token { tokenKind = TokenNewline }:rest) -> if length (tokenValue t) > previousIndent
+            then Right $ length (tokenValue t)
+            else Left $ ExpectedIndentation $ head rest
+
+        (t:_) -> Left $ ExpectedDifferentToken TokenNewline t
+
+    let parseNext acc remaining = case remaining of
            [] -> Right (reverse acc, [])
-           (tok:rest)
-               | tokenKind tok == TokenNewline && length (tokenValue tok) >= minimumIndent -> do
-                  (item, rest') <- runParser itemParser rest
-                  parseNext (item:acc) rest'
-               | otherwise -> Right (reverse acc, remaining)
-   parseNext [] tokens
+           (tok:rest) ->
+                case rest of
+                    [] -> Right (reverse acc, remaining)
+                    [Token { tokenKind = TokenEOF }] -> Right (reverse acc, remaining)
+                    (incoming:_) -> 
+                        let kind = tokenKind tok
+                            isNewline = kind == TokenNewline
+                            tokenIndentation = length (tokenValue tok)
+                        in if isNewline then case compare tokenIndentation indentation of
+                            EQ -> do
+                                (item, rest') <- runParser itemParser rest
+                                parseNext (item:acc) rest'
+                            LT ->   if tokenIndentation == previousIndent then Right (reverse acc, remaining)
+                                    else Left $ ExpectedDifferentIndentation incoming indentation tokenIndentation
+                            GT -> do Left $ ExpectedDifferentIndentation incoming indentation tokenIndentation
+                        else if kind == TokenEOF then Right (reverse acc, remaining)
+                        else do Left $ UnseparatedStatements tok
+
+    parseNext [] tokens
 
 parseBinaryOp :: Parser Expression -> [TokenKind] -> Parser Expression
 parseBinaryOp term operatorTokens = do
