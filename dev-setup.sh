9ada179c42cbd8ea865e738571df91d0f271a46b
Gabriel <gabriel@minatel.dev>
2025-12-26 23:51:41 -0300

    fix(ci): lake builds

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,10 +2,10 @@
 echo "Installing somac..."
 lake build somac
 
-cp -f ./compiler-lean/.lake/build/bin/somac ~/.sup/bin/somac
+ln -sf ./compiler-lean/.lake/build/bin/somac ~/.sup/bin/somac
 
 # Install souls
 echo "Installing souls..."
 lake build souls
 
-cp -f ./souls-lean/.lake/build/bin/souls ~/.sup/bin/souls
+ln -sf ./souls-lean/.lake/build/bin/souls ~/.sup/bin/souls
