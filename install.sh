7dbc577a97f63d0dabaab4339159b012cd21cc2c
Gabriel <gabriel@minatel.dev>
2026-01-04 15:44:24 -0300

    refactor: switch priorities

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
-# Installs haoma (Rust) and souls (Haskell) to PATH
-
-set -e
-
-echo "Starting installation of soma ecosystem..."
-
 # Install somac
 echo "Installing somac..."
-cabal install somac --overwrite-policy=always
+lake build somac
+
+cp -f ./compiler/.lake/build/bin/somac ~/.sup/bin/somac
 
 # Install haoma
 echo "Installing haoma..."
 cargo install --path haoma
 
+cp -f ./haoma/target/release/haoma ~/.sup/bin/haoma
+
 # Install souls
 echo "Installing souls..."
-cabal install souls --overwrite-policy=always
+lake build souls
 
-echo "Soma ecosystem installation complete!"
+cp -f ./souls/.lake/build/bin/souls ~/.sup/bin/souls
